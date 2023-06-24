dad81d152a53ceb2632cfd31d892b6cc5e44b8c0
Daniel <swammeeboy@gmail.com>
2023-06-24 11:24:59 -0700

    Added in a contact form; needs better frontend

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import emailjs from '@emailjs/browser';
+
+// npm i @emailjs/browser
 
 const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_mfaie5e', 'template_e2bmfsv', form.current, 'UwWYiIhpXcNiWeZ0p')
+      .then((result) => {
+          console.log(result.text);
+          console.log("message sent");
+          e.target.reset();
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
     <div className='contact'>
       <div className="container">
         <div className='content'>
-            <h2><span>Contact</span> Page</h2>
-            <p>Here is a bunch of blank stuff that can be deleted when the real contact form is added</p>
+            <form ref={form} onSubmit={sendEmail}>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <br></br>
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <br></br>
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value="Send" />
+            </form>
         </div>
       </div>
     </div>
